dcaf544a2e8fa543674c151156dd331e1b0d984f
Nahuel <nahuemarcilli@gmail.com>
2025-12-11 00:14:22 -0300

    clean structure

--- progiii-api/server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const app = require('./src/app');
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
-});
